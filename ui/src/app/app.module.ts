a5f68d4e4feb086f66aea016e35522932e8e4020
Kendall Chen <Kendall.Chen@kp.org>
2023-06-07 11:48:56 -0700

    update header and footer

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -6,11 +6,12 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MaterialModule } from './common/material.module';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CustomerComponent } from './customer/customer.component';
 import { CustomerDialog } from './customer/dialog/customer-dialog.component';
 import { AppConfigService } from './service/app-config.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 export function initConfig(appConfig: AppConfigService) {
     return () => appConfig.load();
@@ -31,7 +32,8 @@ const routes: Routes = [{ path: '', component: CustomerComponent
   ],
   declarations: [
     AppComponent,
-    TopBarComponent,
+    HeaderComponent,
+    FooterComponent,
     CustomerComponent,
     CustomerDialog
   ],
